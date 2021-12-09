d38426ce67c4bb1eb6cd7c8c38ef6ade4fbd5e6a
Thibaut SIGUENZA <tsiguenz@e1r2p19.42.fr>
2021-12-09 16:15:38 +0100

    all test ok except timeout

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsiguenz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 12:10:39 by tsiguenz          #+#    #+#             */
+/*   Updated: 2021/12/06 17:02:03 by tsiguenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsiguenz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 12:10:44 by tsiguenz          #+#    #+#             */
+/*   Updated: 2021/12/07 23:17:57 by tsiguenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	char	*dup;
+
+	i = 0;
+	dup = ft_calloc((ft_strlen(s) + 1), sizeof(char));
+	if (!dup)
+		return (0);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	return (dup);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	i;
+	void	*ptr;
+
+	i = 0;
+	if (!nmemb || !size)
+		return (0);
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
+	while (i < (nmemb * size))
+	{
+		*(char *)(ptr + i) = 0;
+		i++;
+	}
+	return (ptr);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	res = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	if (!res)
+		return (0);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (res);
+}
